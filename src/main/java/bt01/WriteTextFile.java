4fc91f8e502cd89eb5ca0c73c89323474189de4b
ADMIN <ADMIN@PC344>
2024-09-04 11:29:52 +0700

    bt09

---
+++ src/main/java/bt01/WriteTextFile.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bt01;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class WriteTextFile {
+    /*
+    * @param args the command line arguments 
+    */
+    public static void main(String[] args){
+        ArrayList<SanPham> ds=new ArrayList<>();
+        ds.add(new SanPham ("SP01", "Gao", 25000));
+        ds.add(new SanPham ("SP02", "Đuong", 45000));
+        ds.add(new SanPham ("SP03", "Keo", 29000));
+        
+        try {
+            //Tao luong ghi du lieu ra tap tin van ban
+            FileWriter fw = new FileWriter("SanPham.txt");
+            
+            for (SanPham sp: ds)
+            {
+                //Ghi du lieu ra file 
+                fw.write(sp.getMaso() + ";" +sp.getTen() + ";" +sp.getGia() + "\n");
+            }
+            //Dong luong
+            fw.close();
+            System.out.println("Ghi thanh cong");
+        }catch (Exception ex) {
+            System.out.println("Loi xay ra:" + ex.toString());
+            System.out.println("Ghi file that bai");
+        }
+    }
+}
